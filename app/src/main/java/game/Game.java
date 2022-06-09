3f49ea1958d3e07be01938e40b020c9977b7a7f1
Chris Middleton <ctjmiddleton@gmail.com>
2022-06-09 17:52:54 +0100

    added dictiobary constant and randomise

--- app/src/main/java/game/Game.java
+++ app/src/main/java/game/Game.java
@@ -1,21 +1,23 @@
 package game;
 
+import java.util.Random;
+
 public class Game {
     String word;
     int turnsLeft = 10;
+    static final String[] DICTIONARY = { "MAKERS", "CANDIES", "DEVELOPER", "LONDON"};
 
-    public Game( String guessWord) {
-        this.word = guessWord;
-        
+    public Game() {
+        this.word = this.getRandomWordFromDictionary();
     }
-    
+
     public String getWordToGuess() {
-        String [] splitWord = this.word.split("");
+        String[] splitWord = this.word.split("");
         StringBuilder sb = new StringBuilder("");
 
         sb.append(splitWord[0]);
 
-        for(int i = 0; i < splitWord.length - 1 ; ++i) {
+        for (int i = 0; i < splitWord.length - 1; ++i) {
             sb.append("_");
         }
         return sb.toString();
@@ -24,9 +26,15 @@ public class Game {
     public Integer getRemainingAttempts() {
         return this.turnsLeft;
     }
-    
+
+    public String getRandomWordFromDictionary() {
+        Random generator = new Random();
+        int rnd = generator.nextInt(DICTIONARY.length);
+        return DICTIONARY[rnd];
+    }
+
     // public static void main(String args[]) {
-    //     Game game = new Game("MAKERS");
-    //     System.out.println(game.getWordToGuess());
+    // Game game = new Game("MAKERS");
+    // System.out.println(game.getWordToGuess());
     // }
 }
